ef38fc6e9289292def3a6ad79a95ffe74886e2a2
Daniel Kraft <d@domob.eu>
2019-02-11 10:48:58 +0100

    Initial commit with basic autotools.
    Start the repository with a basic autotools build system and for now an
    "empty" xid binary (that just returns success).

---
+++ README.md
@@ -0,0 +1,1 @@
+# Xaya ID (xid)
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <cstdlib>
+
+int
+main (int argc, char** argv)
+{
+  return EXIT_SUCCESS;
+}
