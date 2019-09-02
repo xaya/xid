9a16e26358a3bd0800e2b5c009e0ec3e99313ea2
Daniel Kraft <d@domob.eu>
2019-09-02 15:00:15 +0200

    Integration test for crypto addresses.
    The new address_update.py integration test verifies that basic updates
    (inserts and deletes) of crypto-address associations work as expected.

---
+++ gametest/address_update.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2019 The Xaya developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from xidtest import XidTest
+
+"""
+Tests basic move processing for updating the crypto-address associations.
+"""
+
+
+class AddressUpdateTest (XidTest):
+
+  def expectAddresses (self, expected):
+    """
+    Retrieves the current game state and asserts that the addresses
+    associated to names match the ones given in the expected dictionary.
+    """
+
+    nmState = self.getGameState ()["names"]
+    self.assertEqual (len (nmState), len (expected))
+
+    for nm, s in nmState.iteritems ():
+      assert nm in expected
+      self.assertEqual (s["addresses"], expected[nm])
+
+  def run (self):
+    self.generate (101)
+    self.expectAddresses ({})
+
+    self.sendMove ("foo", {
+      "ca": {"btc": "1foo", "eth": "0xfoo"},
+    })
+    self.sendMove ("bar", {
+      "ca": {"btc": None, "chi": "Cabcdef"},
+    })
+    self.generate (1)
+    self.expectAddresses ({
+      "foo":
+        {
+          "btc": "1foo",
+          "eth":  "0xfoo",
+        },
+      "bar":
+        {
+          "chi": "Cabcdef",
+        },
+    })
+
+    self.sendMove ("foo", {
+      "ca": {"eth": None},
+    })
+    self.sendMove ("bar", {
+      "ca": {"chi": None},
+    })
+    self.generate (1)
+    self.expectAddresses ({
+      "foo":
+        {
+          "btc": "1foo",
+        },
+    })
+
+
+if __name__ == "__main__":
+  AddressUpdateTest ().main ()
