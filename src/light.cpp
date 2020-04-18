a6b46b1ca79fe8dea313b7d9d0fe39bdec834ebb
Daniel Kraft <d@domob.eu>
2020-04-18 19:30:52 +0200

    Implement getnamestate in light mode.
    Support getnamestate in light mode.  This forwards the request to
    a configured REST API endpoint, and just returns the result gotten
    from there back to the caller.

--- src/light.cpp
+++ src/light.cpp
@@ -13,6 +13,11 @@
 
 #include <google/protobuf/stubs/common.h>
 
+#include <curl/curl.h>
+
+#include <json/json.h>
+#include <jsonrpccpp/common/errors.h>
+#include <jsonrpccpp/common/exception.h>
 #include <jsonrpccpp/server.h>
 #include <jsonrpccpp/server/connectors/httpserver.h>
 
@@ -20,9 +25,12 @@
 #include <cstdlib>
 #include <iostream>
 #include <mutex>
+#include <sstream>
 
 DEFINE_int32 (game_rpc_port, 0,
               "the port at which xid's JSON-RPC server will be started");
+DEFINE_string (rest_endpoint, "",
+               "the endpoint of the REST API that is used to query state");
 
 namespace xid
 {
@@ -81,6 +89,184 @@ public:
   }
 
 };
+
+/**
+ * Simple wrapper around the cURL easy interface to request data from
+ * an HTTP GET endpoint (i.e. the REST API).
+ */
+class CurlRequest
+{
+
+private:
+
+  /** The underlying cURL handle.  */
+  CURL* handle;
+
+  /** Error buffer.  */
+  std::string errBuffer;
+
+  /** Actual error message (from cURL or us).  */
+  std::ostringstream error;
+
+  /** Buffer into which the response data is saved.  */
+  std::string data;
+
+  /** The parsed data as JSON value.  */
+  Json::Value jsonData;
+
+  /**
+   * Sets a given option on the cURL handle.
+   */
+  template <typename T>
+    void
+    SetOption (CURLoption option, const T& param)
+  {
+    CHECK_EQ (curl_easy_setopt (handle, option, param), CURLE_OK);
+  }
+
+  /**
+   * cURL write callback that saves the bytes into our data string.
+   */
+  static size_t
+  WriteCallback (const char* ptr, const size_t sz, const size_t n,
+                 CurlRequest* self)
+  {
+    CHECK_EQ (sz, 1);
+    self->data.append (ptr, n);
+    return n;
+  }
+
+public:
+
+  CurlRequest ();
+
+  ~CurlRequest ()
+  {
+    curl_easy_cleanup (handle);
+  }
+
+  /**
+   * Performs URL encoding of a string.
+   */
+  std::string
+  UrlEncode (const std::string& str)
+  {
+    char* ptr = curl_easy_escape (handle, str.data (), str.size ());
+    CHECK (ptr != nullptr);
+    std::string res(ptr);
+    curl_free (ptr);
+    return res;
+  }
+
+  /**
+   * Start a request to the given URL.  Returns true on success and false
+   * if something went wrong.
+   */
+  bool Request (const std::string& url);
+
+  /**
+   * Returns the data for success.
+   */
+  const Json::Value&
+  GetData () const
+  {
+    return jsonData;
+  }
+
+  /**
+   * Returns the error message in case of an error.
+   */
+  std::string
+  GetError () const
+  {
+    return error.str ();
+  }
+
+};
+
+CurlRequest::CurlRequest ()
+{
+  handle = curl_easy_init ();
+  CHECK (handle != nullptr);
+
+  /* Let cURL store error messages into our error string.  */
+  errBuffer.resize (CURL_ERROR_SIZE);
+  SetOption (CURLOPT_ERRORBUFFER, errBuffer.data ());
+
+  /* Enforce TLS verification.  */
+  SetOption (CURLOPT_SSL_VERIFYPEER, 1L);
+  SetOption (CURLOPT_SSL_VERIFYHOST, 2L);
+
+  /* Install our write callback.  */
+  SetOption (CURLOPT_WRITEFUNCTION, &WriteCallback);
+  SetOption (CURLOPT_WRITEDATA, this);
+}
+
+bool
+CurlRequest::Request (const std::string& url)
+{
+  VLOG (1) << "Requesting data from " << url << "...";
+
+  data.clear ();
+  SetOption (CURLOPT_URL, url.c_str ());
+
+  if (curl_easy_perform (handle) != CURLE_OK)
+    {
+      LOG (WARNING)
+          << "cURL request for " << url << " failed: " << errBuffer.c_str ();
+      error << "cURL error: " << errBuffer.c_str ();
+      return false;
+    }
+
+  long code;
+  CHECK_EQ (curl_easy_getinfo (handle, CURLINFO_RESPONSE_CODE, &code),
+            CURLE_OK);
+
+  if (code != 200)
+    {
+      LOG (WARNING)
+          << "cURL request for " << url << " returned status: " << code;
+      error << "HTTP response status: " << code;
+      return false;
+    }
+
+  VLOG (1) << "Request successful";
+  VLOG (2) << "Return data:\n" << data;
+
+  Json::CharReaderBuilder rbuilder;
+  rbuilder["allowComments"] = false;
+  rbuilder["strictRoot"] = true;
+  rbuilder["allowDroppedNullPlaceholders"] = false;
+  rbuilder["allowNumericKeys"] = false;
+  rbuilder["allowSingleQuotes"] = false;
+  rbuilder["failIfExtra"] = true;
+  rbuilder["rejectDupKeys"] = true;
+  rbuilder["allowSpecialFloats"] = false;
+
+  std::string parseErrs;
+  std::istringstream in(data);
+  try
+    {
+      if (!Json::parseFromStream (rbuilder, in, &jsonData, &parseErrs))
+        {
+          LOG (WARNING)
+              << "Failed to parse response data as JSON: " << parseErrs
+              << "\n" << data;
+          error << "JSON parser: " << parseErrs;
+          return false;
+        }
+    }
+  catch (const Json::Exception& exc)
+    {
+      LOG (WARNING)
+          << "JSON parser threw: " << exc.what ()
+          << "\n" << data;
+      error << "JSON parser: " << exc.what ();
+      return false;
+    }
+
+    return true;
+}
 
 /**
  * The main RPC server implementation for the light API.
@@ -142,8 +328,17 @@ Json::Value
 LightServer::getnamestate (const std::string& name)
 {
   LOG (INFO) << "RPC method called: getnamestate " << name;
-  /* FIXME: Implement using the REST API.  */
-  return Json::Value ();
+
+  CurlRequest req;
+  std::ostringstream url;
+  url << FLAGS_rest_endpoint << "/name/";
+  url << req.UrlEncode (name);
+
+  if (!req.Request (url.str ()))
+    throw jsonrpc::JsonRpcException (jsonrpc::Errors::ERROR_RPC_INTERNAL_ERROR,
+                                     req.GetError ());
+
+  return req.GetData ();
 }
 
 } // anonymous namespace
@@ -154,6 +349,7 @@ main (int argc, char** argv)
 {
   google::InitGoogleLogging (argv[0]);
   GOOGLE_PROTOBUF_VERIFY_VERSION;
+  CHECK_EQ (curl_global_init (CURL_GLOBAL_ALL), 0);
 
   gflags::SetUsageMessage ("Run Xaya ID light interface");
   gflags::SetVersionString (PACKAGE_VERSION);
@@ -164,12 +360,18 @@ main (int argc, char** argv)
       std::cerr << "Error: --game_rpc_port must be specified" << std::endl;
       return EXIT_FAILURE;
     }
+  if (FLAGS_rest_endpoint.empty ())
+    {
+      std::cerr << "Error: --rest_endpoint must be specified" << std::endl;
+      return EXIT_FAILURE;
+    }
 
   xid::MainLoop loop;
 
   jsonrpc::HttpServer http(FLAGS_game_rpc_port);
   xid::LightServer srv(loop, http);
 
+  LOG (INFO) << "Using REST API at " << FLAGS_rest_endpoint;
   LOG (INFO) << "Starting local RPC server on port " << FLAGS_game_rpc_port;
   srv.StartListening ();
 
