63f4c435e99fbbacb1b8e3beddaca132a66ed2e8
Daniel Kraft <d@domob.eu>
2020-09-29 09:58:32 +0200

    Use libxayagame's new RestApi.
    A lot of the basic code from RestApi has been moved to libxayagame, so
    that it can be shared with other GSPs.  This allows us to use it from
    there, and just implement the actual xid-specific request processing
    here.

--- src/rest.hpp
+++ src/rest.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 The Xaya developers
+// Copyright (C) 2019-2020 The Xaya developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,14 +7,8 @@
 
 #include "logic.hpp"
 
-#include <xayagame/defaultmain.hpp>
 #include <xayagame/game.hpp>
-
-#include <json/json.h>
-
-#include <microhttpd.h>
-
-#include <string>
+#include <xayagame/rest.hpp>
 
 namespace xid
 {
@@ -22,13 +16,10 @@ namespace xid
 /**
  * HTTP server providing a REST API for reading xid data.
  */
-class RestApi : public xaya::GameComponent
+class RestApi : public xaya::RestApi
 {
 
 private:
-
-  /** Exception class thrown for returning HTTP errors to the client.  */
-  class HttpError;
 
   /** The underlying Game instance that manages everything.  */
   xaya::Game& game;
@@ -36,49 +27,15 @@ private:
   /** The game logic implementation.  */
   XidGame& logic;
 
-  /** The port to listen on.  */
-  const int port;
+protected:
 
-  /** The underlying MDH daemon.  */
-  struct MHD_Daemon* daemon = nullptr;
-
-  /**
-   * Internal request handler function.  It should return the JSON value
-   * we want to send on success, or throw an HttpError instance.
-   */
-  Json::Value Process (const std::string& url);
-
-  /**
-   * Request handler function for MHD.
-   */
-  static int RequestCallback (void* data, struct MHD_Connection* conn,
-                              const char* url, const char* method,
-                              const char* version,
-                              const char* upload, size_t* uploadSize,
-                              void** connData);
+  SuccessResult Process (const std::string& url) override;
 
 public:
 
   explicit RestApi (xaya::Game& g, XidGame& l, const int p)
-    : game(g), logic(l), port(p)
+    : xaya::RestApi(p), game(g), logic(l)
   {}
-
-  ~RestApi ();
-
-  RestApi () = delete;
-  RestApi (const RestApi&) = delete;
-  void operator= (const RestApi&) = delete;
-
-  /**
-   * Starts the REST server.  Processing of requests is done in a separate
-   * thread, so this method returns immediately.
-   */
-  void Start () override;
-
-  /**
-   * Shuts down the REST server.
-   */
-  void Stop () override;
 
 };
 
